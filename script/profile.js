192f4f7c2fdb05e64d7106335939d6b4e32723d7
Eman Butt <butt.eman391@gmail.com>
2025-04-20 17:30:02 +0500

    Save button completed.

--- script/profile.js
+++ script/profile.js
@@ -4,5 +4,15 @@ $(function(){
 
     $("#name").val(username);
 
+    $("#name").on("input", function(){
+        $("#save-btn").show();
+    });
+
+    $("#save-btn").on("click", function(){
+        let newUsername = $("#name").val();
+        localStorage.setItem("username", newUsername);
+        $("#save-btn").hide();
+    });
+
     
 });
